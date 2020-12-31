18b806a0bb15165f6396562bdcbefb2e13d32f57
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-12-31 15:35:28 +0530

    initial commit

---
+++ src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
@@ -0,0 +1,19 @@
+package simplest.possible.implementation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.sun.tools.javac.util.List.of;
+import static java.util.Collections.emptyList;
+
+public class PrimeFactorsTest {
+    @Test
+    void thereAreNoPrimeFactorsFor1() {
+        Assertions.assertEquals(emptyList(), new PrimeFactors().of(1));
+    }
+
+    @Test
+    void primeFactorOf2Is2Itself() {
+        Assertions.assertEquals(of(2), new PrimeFactors().of(2));
+    }
+}
